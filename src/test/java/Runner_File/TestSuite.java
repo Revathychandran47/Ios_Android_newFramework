e7d2728f55165a8bee059859fbd678b52ec327e6
Revathy chandran <53893307+Revathychandran47@users.noreply.github.com>
2022-06-21 18:21:32 +0530

    Add files via upload

---
+++ src/test/java/Runner_File/TestSuite.java
@@ -0,0 +1,14 @@
+package Runner_File;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        publish = true,
+        plugin = {"pretty","html:target/site/serenity/Report.html"},
+        features = "src/test/resources/features/01_Landing_positive_scenario.feature",glue= {"Stepdefinition"})
+public class TestSuite {
+
+}
